da659e8c8e798576fbc365fa378019c2164137ce
Darrel Herbst <dherbst@gmail.com>
2015-08-30 09:05:54 -0400

    adding test for version handler

--- www/handlers.go
+++ www/handlers.go
@@ -1,14 +1,34 @@
 package slackbot
 
 import (
+	"encoding/json"
 	"net/http"
 )
 
+var version string = "1.0.0"
+
 func init() {
 	http.HandleFunc("/api/1/command", CommandHandler)
+	http.HandleFunc("/version", VersionHandler)
 	http.HandleFunc("/", IndexHandler)
 }
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/index.html", http.StatusFound)
+}
+
+func VersionHandler(w http.ResponseWriter, r *http.Request) {
+
+	value := map[string]interface{}{
+		"Version": version,
+	}
+
+	js, err := json.Marshal(value)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 }
---
+++ www/handlers_test.go
@@ -0,0 +1,39 @@
+package slackbot
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestVersionHandler(t *testing.T) {
+	request, err := http.NewRequest("GET", "/api/1/verison", nil)
+	if err != nil {
+		t.FailNow()
+	}
+	response := httptest.NewRecorder()
+
+	VersionHandler(response, request)
+
+	if response.Code != http.StatusOK {
+		t.Fatalf("Status from GET did not return 200, instead returned %v", response.Code)
+		t.FailNow()
+	}
+
+	versionBody, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		t.FailNow()
+	}
+	var obj map[string]interface{}
+	err = json.Unmarshal(versionBody, &obj)
+	if err != nil {
+		t.Fatalf("Could not unmarshal version body %v", versionBody)
+		t.FailNow()
+	}
+	if obj["Version"] != "1.0.0" {
+		t.Fatalf("Version=%v", obj["Version"])
+		t.FailNow()
+	}
+}
